c3602a1b450fe5e5c9950e57c5d904a447d25b92
FlowXII <zidany.mehdi@gmail.com>
2024-07-18 15:48:05 +0200

    deploy finish

--- back/server.js
+++ back/server.js
@@ -1,10 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 dotenv.config();
 
-// Unused for now
-// import protectedRoute from './routes/protectedRoute';
 
 import tournamentsUserRoute from './routes/tournamentsUserRoute.js';
 import tournamentsStationsRoute from './routes/tournamentsStationsRoute.js';
@@ -16,20 +15,20 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-// app.use(tokenRoutes);
-
 app.use('/api', tournamentsUserRoute);
 app.use('/api', tournamentsStationsRoute);
 app.use('/api', tournamentsUpcomingRoute);
 
-// Other imports and code remain unchanged
+app.use(express.static(path.join(__dirname, '../front/dist')));
 
-// Comment out or remove the app.listen part when running tests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front/dist', 'index.html'));
+});
+
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
 
-// Export the app for testing
 export default app;
