bb76c11f2d7ad529a74392584f082bb58ff9ac10
Mehdi Zidany <120339995+FlowXII@users.noreply.github.com>
2024-09-18 14:35:22 +0000

    fixing auth route messed up

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,16 +4,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'My React App',
-        short_name: 'ReactApp',
-        description: 'My React App description',
-        theme_color: '#ffffff'
-        // Icons can be omitted if not needed
-      }
-    })
+    react()
   ]
 });
