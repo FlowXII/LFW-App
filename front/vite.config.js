748b33c36860382c56a7b076a36b93dede8fbe33
Mehdi Zidany <120339995+FlowXII@users.noreply.github.com>
2024-09-18 13:41:54 +0000

    add vitepwa to vite config and add config

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,5 +4,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), 
+    VitePWA({
+    injectRegister: 'auto'
+  })
+],
 })
